homalg-project/FinSetsForCAP

commit 798c9fb3150d19ddc99745a9e0543ccd119269b8

simplified CartesianEvaluationMorphismWithGivenSource
for SkeletalFinSets

gap/SkeletalFinSetsForCAP.gi
@@ -802,12 +802,13 @@ end );
 ##
 AddCartesianBraidingInverseWithGivenDirectProducts( SkeletalFinSets,
   function ( cat, MN, M, N, NM )
-    local m, n;
+    local n, mn;
     
-    m := Length( M );
     n := Length( N );
     
-    return MapOfFinSets( MN, List( AsList( MN ), k -> ( k-1 ) mod n * m + Int( ( k-1 ) / n ) + 1 ), NM );
+    mn := Length( MN );
+    
+    return MapOfFinSets( MN, List( [ 0 .. mn - 1 ] , i -> 1 + ( i mod mn ) + QuoInt( i, n ) ), NM );
     
 end );
 

gap/precompiled_categories/CategoryOfSkeletalFinSetsPrecompiled.gi
@@ -10,13 +10,12 @@ BindGlobal( "ADD_FUNCTIONS_FOR_CategoryOfSkeletalFinSetsPrecompiled", function (
         
 ########
 function ( cat_1, s_1, a_1, b_1, r_1 )
-    local hoisted_1_1, hoisted_2_1;
-    hoisted_2_1 := Length( a_1 );
-    hoisted_1_1 := Length( b_1 );
-    return MapOfFinSets( s_1, List( AsList( s_1 ), function ( k_2 )
-              local deduped_1_2;
-              deduped_1_2 := k_2 - 1;
-              return deduped_1_2 mod hoisted_1_1 * hoisted_2_1 + Int( deduped_1_2 / hoisted_1_1 ) + 1;
+    local hoisted_1_1, hoisted_2_1, deduped_3_1;
+    deduped_3_1 := Length( s_1 );
+    hoisted_2_1 := Length( b_1 );
+    hoisted_1_1 := deduped_3_1;
+    return MapOfFinSets( s_1, List( [ 0 .. deduped_3_1 - 1 ], function ( i_2 )
+              return 1 + i_2 mod hoisted_1_1 + QUO_INT( i_2, hoisted_2_1 );
           end ), r_1 );
 end
 ########

commit bb503351962ac318d88afc47034146da0b129146

fixed AddCartesianBraidingInverseWithGivenDirectProducts( SkeletalFinSets, ... )
this bug was introduced in commit 798c9fb3150d19ddc99745a9e0543ccd119269b8,
a commit that had a misleading commit message had a misleading commit message

PackageInfo.g
@@ -10,7 +10,7 @@ SetPackageInfo( rec(
 
 PackageName := "FinSetsForCAP",
 Subtitle := "The elementary topos of (skeletal) finite sets",
-Version := "2021.12-07",
+Version := "2021.12-08",
 
 Date := Concatenation( "01/", ~.Version{[ 6, 7 ]}, "/", ~.Version{[ 1 .. 4 ]} ),
 License := "GPL-2.0-or-later",

gap/SkeletalFinSetsForCAP.gi
@@ -804,13 +804,13 @@ end );
 ##
 AddCartesianBraidingInverseWithGivenDirectProducts( SkeletalFinSets,
   function ( cat, MN, M, N, NM )
-    local n, mn;
+    local m, n;
+    
+    m := Length( M );
     
     n := Length( N );
     
-    mn := Length( MN );
-    
-    return MapOfFinSets( cat, MN, List( [ 0 .. mn - 1 ] , i -> 1 + ( i mod mn ) + QuoInt( i, n ) ), NM );
+    return MapOfFinSets( cat, MN, List( [ 0 .. Length( MN ) - 1 ] , i -> 1 + ( i mod n ) * m + QuoInt( i, n ) ), NM );
     
 end );
 

gap/precompiled_categories/CategoryOfSkeletalFinSetsPrecompiled.gi
@@ -10,13 +10,12 @@ BindGlobal( "ADD_FUNCTIONS_FOR_CategoryOfSkeletalFinSetsPrecompiled", function (
         
 ########
 function ( cat_1, s_1, a_1, b_1, r_1 )
-    local hoisted_1_1, hoisted_2_1, deduped_3_1;
-    deduped_3_1 := Length( s_1 );
-    hoisted_2_1 := Length( b_1 );
-    hoisted_1_1 := deduped_3_1;
+    local hoisted_1_1, hoisted_2_1;
+    hoisted_2_1 := Length( a_1 );
+    hoisted_1_1 := Length( b_1 );
     return ObjectifyMorphismWithSourceAndRangeForCAPWithAttributes( rec(
-           ), cat_1, s_1, r_1, AsList, List( [ 0 .. deduped_3_1 - 1 ], function ( i_2 )
-              return 1 + i_2 mod hoisted_1_1 + QUO_INT( i_2, hoisted_2_1 );
+           ), cat_1, s_1, r_1, AsList, List( [ 0 .. Length( s_1 ) - 1 ], function ( i_2 )
+              return 1 + i_2 mod hoisted_1_1 * hoisted_2_1 + QUO_INT( i_2, hoisted_1_1 );
           end ) );
 end
 ########